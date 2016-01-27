10dd7f744d92c7db1bb0f11ba07923964889fe00
leerduo <eoeduo@163.com>
2016-01-27 10:11:28 +0800

    AndroidActivityGraph

--- dagger/src/main/java/me/jarvischen/dagger/AndroidModule.java
+++ dagger/src/main/java/me/jarvischen/dagger/AndroidModule.java
@@ -13,6 +13,8 @@ import static android.content.Context.LOCATION_SERVICE;
 /**
  * Created by chenfuduo on 2016/1/26.
  */
+//在Module中若provide的类，没有在injects列表中使用， 将在编译时触发错误。
+//若是这个Module提供的对象绑定， 可能被injects列表中以外的类使用， 可以将改Module标记为library, 以避免出错。
 @Module(library = true)
 public class AndroidModule {
     private final MyApp app;
--- dagger/src/main/java/me/jarvischen/dagger/DaggerDemoModule.java
+++ dagger/src/main/java/me/jarvischen/dagger/DaggerDemoModule.java
@@ -6,8 +6,14 @@ import dagger.Module;
  * Created by chenfuduo on 2016/1/26.
  */
 
+//显示声明我们需要注入MyBaseActivity这个类
+//Dagger包含一个annotation 处理器， 这个处理器检查module和注入的有效性。
+// 处理器非常严格， 若是有任何绑定是无效或者不完整的， 将引发编译错误。
+//此处标记DaggerDemoModule是不完整的
 
-@Module(injects = MyBaseActivity.class, complete = false)
+@Module(
+        injects = MyBaseActivity.class,
+        complete = false)
 
 public class DaggerDemoModule {
 }
